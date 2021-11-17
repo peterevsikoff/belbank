1011ac7f8de72961bfa64ca977893b6c67349a90
peterevsikoff <93715088+peterevsikoff@users.noreply.github.com>
2021-11-17 21:08:59 +0200

    Add files via upload

---
+++ Models/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BelBank.Models
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            Database.EnsureCreated();   // создаем базу данных при первом обращении
+        }
+    }
+}
